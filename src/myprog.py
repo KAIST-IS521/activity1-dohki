76d75f9fe99204e485fb4cca9493dde34ec7f906
Dohyeok Kim <kr.dohki@gmail.com>
2019-03-07 21:09:03 +0900

    Add src/myprog.py

---
+++ src/myprog.py
@@ -0,0 +1,27 @@
+import sys
+import strictcsv as scsv
+            
+
+def main():
+    assert len(sys.argv) == 3
+    [file_name, col_idx] = sys.argv[1:]
+    #TODO: Validate col_idx
+
+    try:
+        sc = scsv.StrictCsv(file_name)
+        nth_col = sc.get_nth_col(int(col_idx))
+
+        for row in nth_col:
+            print(row)
+
+    except scsv.NotSameColNumError:
+        print('[*] Given csv file does not have same column number for all rows.')
+        sys.exit(1)
+
+    except scsv.ColIdxOutOfBoundError:
+        print('[*] Given column index exceeds the bound.')
+        sys.exit(1)
+
+
+if __name__ == '__main__':
+    main()
